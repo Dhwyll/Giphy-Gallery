4389a6b1d5cc3f1890f927fbf5817d30be1388f7
Brian Evans <dhwyll@gmail.com>
2017-08-26 16:24:00 -0700

    Mobile fixes not working, revering to just desktop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -103,7 +103,7 @@ $(document).ready(function(){
 
 	renderButtons();																// Render the buttons at the beginning
 
-	$(document).on("click tap", ".gifSearch", displayGIFInfo);							// When a GIF Item is clicked, display its results
-	$(document).on("click tap", ".gifPic", gifAnimation);
+	$(document).on("click", ".gifSearch", displayGIFInfo);						// When a search term is clicked, display its results
+	$(document).on("click", ".gifPic", gifAnimation);							// When a GIF is clicked, animate it
 	  
 });
